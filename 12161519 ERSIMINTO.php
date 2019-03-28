ecd92fe47de2c982d9ef6fd187115013a2f65cb5
ersibkl <49021644+ersibkl@users.noreply.github.com>
2019-03-28 16:00:06 +0700

    Add files via upload
    12161519
    ersiminto

---
+++ 12161519 ERSIMINTO.php
@@ -0,0 +1,16 @@
+<?php
+
+	$ikan = [
+		['nila' => ['warna' => 'abu', 'ukuran' => '1 kg', 'hidup' => 'tawar']], 
+		['mas' => ['warna' => 'kuning', 'ukuran' => '2kg', 'hidup' => 'tawar']], 
+		['lele' => ['warna' => 'hitam', 'ukuran' => '1 kg', 'hidup' => 'tawar']], 
+		['pari' => ['warna' => 'abu-abu', 'ukuran' => '6 kg','hidup' => 'laut']], 
+		['hiu' => ['warna' => 'biru', 'ukuran' => '53 kg', 'hidup' => 'laut']], 
+		
+	];
+
+	for ($i=0;$i<1;$i++)
+	{
+		echo '<pre>'; print_r($ikan);
+	};
+?>
